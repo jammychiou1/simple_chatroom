d6cacb65a170e0ff134dc8a7a6dadad8c99124ef
jammychiou1 <jammy.chiou1@gmail.com>
2022-01-12 00:01:59 +0800

    retab and add .gitignore

--- main.go
+++ main.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
-	"net"
+    "bufio"
+    "fmt"
+    "io"
+    "log"
+    "net"
 )
 
 func main() {
-	li, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalln(err)
-	}
+    li, err := net.Listen("tcp", ":8080")
+    if err != nil {
+        log.Fatalln(err)
+    }
 
-	defer li.Close()
+    defer li.Close()
 
-	for {
-		conn, err := li.Accept()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		io.WriteString(conn, "\nHello from TCP server\n")
-		// fmt.Fprintln(conn, "How is your day?")
-		// fmt.Fprintf(conn, "%v", "Well, I hope!\n\n")
-		go handle(conn)
-	}
+    for {
+        conn, err := li.Accept()
+        if err != nil {
+            log.Println(err)
+            continue
+        }
+        io.WriteString(conn, "\nHello from TCP server\n")
+        // fmt.Fprintln(conn, "How is your day?")
+        // fmt.Fprintf(conn, "%v", "Well, I hope!\n\n")
+        go handle(conn)
+    }
 }
 
 func handle(conn net.Conn) {
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		ln := scanner.Text()
-		fmt.Println(ln)
-	}
-	defer conn.Close()
+    scanner := bufio.NewScanner(conn)
+    for scanner.Scan() {
+        ln := scanner.Text()
+        fmt.Println(ln)
+    }
+    defer conn.Close()
 
-	fmt.Println("connection interrupt")
+    fmt.Println("connection interrupt")
 }
